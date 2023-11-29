e84415684d25a0ce9ea46b7cfe3b04fe0b61f753
Sagar Giradkar <129714089+Smit-budgetalizer@users.noreply.github.com>
2023-11-29 20:21:24 +0530

    Basic App Created

--- SmartFarmingApp/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-// store.js
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-
-
-const store = createStore(rootReducer, applyMiddleware(thunk));
-
-export default store;
